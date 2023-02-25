74c84c1a73b3c244dee4470000c60b02f29104d2
SinugbangIsda <marcuoperario@gmail.com>
2023-02-25 15:07:40 +0800

    refactored getDayPeriod

--- utils/getDayPeriod.ts
+++ utils/getDayPeriod.ts
@@ -7,13 +7,6 @@ const getDayPeriod = ( data: Number ) => {
     setHour(data);
   }, [hour])
 
-  // if (hour < 12) {
-  //   return "Good morning";
-  // } else if (hour < 18) {
-  //   return "Good afternoon";
-  // } else {
-  //   return "Good evening"
-  // }
   return (hour < 12) ? "Good morning" : (hour < 18) ? "Good afternoon": "Good evening"
 
 }
